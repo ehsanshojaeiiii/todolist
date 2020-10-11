08af93b6908b6675c3780c5c414660d2bcdde185
ehsansh10 <ehsan.shojaei78@gmail.com>
2020-10-12 02:42:42 +0330

    adding submit form

--- tasks/forms.py
+++ tasks/forms.py
@@ -4,5 +4,7 @@ from django.forms import ModelForm
 from .models import *
 
 
-# class TaskForm(forms.ModelForm):
-#     class Meta():
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = '__all__'
